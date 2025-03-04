b87b19d1420beb3e86aaa32946d181523993e732
sudarshank2507 <sudarshank2507@gmail.com>
2025-03-05 02:07:30 +0530

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from routes import tasks
 from  routes import auth
 app = FastAPI()
 
+
 Base.metadata.create_all(bind=engine)
 
 app.include_router(tasks.router, prefix="/api")
 app.include_router(auth.router, prefix="/auth")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
